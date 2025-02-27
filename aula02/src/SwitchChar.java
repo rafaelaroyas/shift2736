6cbe424cecc0df7fcaf8a9a1bda9539bbce3c996
rafaelaroyas <rafaela.r.oyas@hotmail.com>
2025-02-26 22:42:51 -0300

    aula 02 atualizacoes

--- aula02/src/SwitchChar.java
+++ aula02/src/SwitchChar.java
@@ -1,7 +1,7 @@
 public class SwitchChar {
     public static void main(String[] args) {
         char sinal = "X";
-        switch (sinal){
+        switch (sinal) {
             case "x":
             case "X":
             case "*":
@@ -11,4 +11,6 @@ public class SwitchChar {
                 System.out.println("Adição");break;
             case "-":
                 System.out.println("Subtração");break;
+        }
+    }
 }
