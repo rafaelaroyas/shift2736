92dea34832c8a4e22ddc4eab1c179ee87c2a1aac
rafaelaroyas <rafaela.r.oyas@hotmail.com>
2025-03-11 19:39:42 -0300

    exercício JUNÇÃO

--- aula04/src/Juncao.java
+++ aula04/src/Juncao.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Juncao {
     public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
         //1. ler 2 vetores de inteiros com 4 posições
         //2. Criar um 3 vetor que é a junção dos dois
         //3. Criar um 4 vetor que será o vetA invertido
@@ -10,7 +13,32 @@ public class Juncao {
         vetD = 3,9,8,2;*/
 
         int[] vetA = new int[4];
+        int[] vetB = new int[4];
+        int i;
+        System.out.println("\nPreenchendo o vetor A");
+        for (i=0; i<vetA.length; i++){
+            System.out.println("Digite o " + (i+1) + "° valor: ");
+            vetA[i] = leitor.nextInt();
+        }
 
-        //int[] vetA = {2,8,9,3};
+        System.out.println("\nPreenchendo o vetor B");
+        for (i=0; i<vetB.length; i++){
+            System.out.println("Digite o " + (i+1) + "° valor: ");
+            vetB[i] = leitor.nextInt();
+        }
+
+        //2. Criar um 3 vetor que é a junção dos dois
+        int[] vetC = new int[8];
+
+        for (i=0; i < vetA.length; i++){
+            vetC[i] = vetA[i];
+            vetC[i+4] = vetB[i];
+        }
+        for (i=0; i < vetC.length; i++){
+            System.out.println(vetC[i]);
+        }
+
+        //3. Criar um 4 vetor que será o vetA invertido
+        int[] vetD = new int[4];
     }
 }
